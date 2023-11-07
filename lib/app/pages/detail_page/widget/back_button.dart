4d90aacfc0d2bb2b2b52cef26de0146fd94bc7ba
Danish Pramuditya <darizq37@gmail.com>
2023-11-07 23:21:46 +0700

    feat: [DN] adding detail page

---
+++ lib/app/pages/detail_page/widget/back_button.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class BackButtonArrow extends StatelessWidget {
+  const BackButtonArrow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          height: 55,
+          width: 55,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              height: 55,
+              width: 55,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const Icon(
+                Icons.arrow_back_ios,
+                size: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
