02a609712d849a161ed481b31008a337e691d6e3
ayla syahira <aylasyafaa23@gmail.com>
2023-11-09 22:39:25 +0700

    feat: [AS] adding profile page

---
+++ lib/app/pages/profile_page/widget/settings_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pas_mobile/common/theme/theme.dart';
+
+class SettingWidget extends StatelessWidget {
+  const SettingWidget({super.key, required this.icon, required this.name});
+
+  final String name;
+  final IconData icon;
+
+@override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          SizedBox(width: 30),
+          Icon(
+            icon,
+            color: ColorsBase.orangeBase,
+          ),
+          SizedBox(width: 15), 
+          Expanded(
+            child: Text(
+              name,
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                color: ColorsBase.purpleLightBase,
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
