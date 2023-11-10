7b79663d6210653588eac7ab120a32d5dc9f47f4
Danish Pramuditya <darizq37@gmail.com>
2023-11-10 19:52:02 +0700

    feat: [DN] change event venue back again

--- lib/app/pages/detail_page/detail_page_view.dart
+++ lib/app/pages/detail_page/detail_page_view.dart
@@ -14,11 +14,8 @@ class DetailPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     final DetailPageController controller = Get.put(DetailPageController());
-    final String eventImageURL =
-        controller.data.value.embedded!.events[0].images[0].url;
+    final String eventImageURL = controller.data.value.embedded!.events[0].images[0].url;
     final String eventName = controller.data.value.embedded!.events[0].name;
-    final String eventVenue =
-        controller.data.value.embedded!.events[0].embedded.venues[0].name;
     return Scaffold(
       body: SafeArea(
         child: Obx(
@@ -39,14 +36,14 @@ class DetailPageView extends StatelessWidget {
                       eventName: eventName,
                       eventPromoter: controller
                           .data.value.embedded!.events[0].promoter.name,
-                      eventVenue: eventVenue,
+                      eventVenue: controller
+                          .data.value.embedded!.events[0].embedded.venues[0].name,
                       eventDate: controller
                           .data.value.embedded!.events[0].dates.start.localDate,
                       eventTime: controller
                           .data.value.embedded!.events[0].dates.start.localTime,
-                      eventInfo:
-                          controller.data.value.embedded!.events[0].info ??
-                              "No Information for this event",
+                      eventInfo: controller
+                          .data.value.embedded!.events[0].info ?? "No Information for this event",
                     ),
                     const BackButtonArrow(),
                     Align(
