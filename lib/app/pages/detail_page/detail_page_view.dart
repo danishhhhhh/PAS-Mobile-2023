5e83cd6ae552d61c20ec61a565b48b9bccc5803f
Danish Pramuditya <darizq37@gmail.com>
2023-11-10 19:42:10 +0700

    feat: [DN] change event name to separated variable

--- lib/app/pages/detail_page/detail_page_view.dart
+++ lib/app/pages/detail_page/detail_page_view.dart
@@ -14,7 +14,8 @@ class DetailPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     final DetailPageController controller = Get.put(DetailPageController());
-    final String imageURL = controller.data.value.embedded!.events[0].images[0].url;
+    final String eventImageURL = controller.data.value.embedded!.events[0].images[0].url;
+    final String eventName = controller.data.value.embedded!.events[0].name;
     return Scaffold(
       body: SafeArea(
         child: Obx(
@@ -27,12 +28,12 @@ class DetailPageView extends StatelessWidget {
                     SizedBox(
                       height: height * 0.4,
                       child: Image.network(
-                        imageURL,
+                        eventImageURL,
                         fit: BoxFit.cover,
                       ),
                     ),
                     BottomContainer(
-                      eventName: controller.data.value.embedded!.events[0].name,
+                      eventName: eventName,
                       eventPromoter: controller
                           .data.value.embedded!.events[0].promoter.name,
                       eventVenue: controller
