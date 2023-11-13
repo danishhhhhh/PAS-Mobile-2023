947a8bd660db162120777568d99ad9a0c6b17f6c
Danish Pramuditya <darizq37@gmail.com>
2023-11-13 08:26:15 +0700

    feat: [DN] add api login

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-
       ),
       getPages: routes,
 
