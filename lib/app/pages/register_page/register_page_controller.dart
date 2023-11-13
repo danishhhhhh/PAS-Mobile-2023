75f98ac24ebea4d416ac376a7a84865f9016229c
Danish Pramuditya <darizq37@gmail.com>
2023-11-13 08:53:25 +0700

    feat: [DN] add api signup

--- lib/app/pages/register_page/register_page_controller.dart
+++ lib/app/pages/register_page/register_page_controller.dart
@@ -1,6 +1,35 @@
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RegisterPageController extends GetxController {
   RxBool isObsecure = true.obs;
   RxBool isObsecureFalse = false.obs;
+  RxBool successfulLogin = false.obs;
+  RxString messageSignUp = "".obs;
+
+  signup(String username, String email, String password) async {
+    final response = await http.post(
+      Uri.parse("https://mediadwi.com/api/latihan/register-user"),
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: <String, String>{
+        'username': username,
+        'full_name': username,
+        'email': email,
+        'password': password,
+      },
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = json.decode(response.body);
+      bool status = jsonResponse['status'];
+      String message = jsonResponse['message'];
+      print("status: ${status}");
+        messageSignUp.value = message;
+        print("message: ${message}");
+    } else {
+      print("status code : ${response.statusCode.toString()}");
+    }
+  }
 }
--- lib/app/pages/register_page/widget/sign_up_button.dart
+++ lib/app/pages/register_page/widget/sign_up_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pas_mobile/app/pages/register_page/register_page_controller.dart';
 import 'package:pas_mobile/common/theme/theme.dart';
 
-class SignUpButton extends StatelessWidget {
+class SignUpButton extends GetView<RegisterPageController> {
   const SignUpButton({super.key, required this.usernameTextEditing, required this.emailTextEditing, required this.passwordTextEditing});
 
   final TextEditingController usernameTextEditing, emailTextEditing, passwordTextEditing;
@@ -13,8 +15,44 @@ class SignUpButton extends StatelessWidget {
       height: 50,
       margin: EdgeInsets.symmetric(horizontal: 60),
       child: ElevatedButton(
-        onPressed: () {
-          print("email: ${emailTextEditing.text} : pass: ${passwordTextEditing.text}");
+        onPressed: () async {
+          if(!(emailTextEditing.text == "" && passwordTextEditing.text == "" && emailTextEditing.text == "")){
+            await controller.signup(
+                emailTextEditing.text, passwordTextEditing.text, emailTextEditing.text);
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Obx(()=>Text(controller.messageSignUp.value)),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          } else {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Text('please fill the fields'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          }
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: ColorsBase.purpleDarkBase
