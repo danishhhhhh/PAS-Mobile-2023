3b45ca3d8eced8cd8494e73f855c79177878a205
ayla syahira <aylasyafaa23@gmail.com>
2023-11-16 14:59:31 +0700

    [AS] add image from local

---
+++ lib/app/pages/profile_page/image_picker_controller.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:get/get.dart';
+
+class ImagePickerController extends GetxController {
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       getPages: routes,
-      initialRoute: "/login",
+      initialRoute: "/profile",
     );
   }
 }
